5f060a33ef66ae14db33325b014bcbc8f7657ffd
Ester <estermolinari135@gmail.com>
2021-12-10 23:16:01 +0100

    Create UDPClient and UDPServer

---
+++ UDPServer/src/UDPServer.c
@@ -0,0 +1,17 @@
+/*
+ ============================================================================
+ Name        : UDPServer.c
+ Author      : Ester Molinari
+ Version     : 1.0
+ Copyright   : Your copyright notice
+ Description : A simple UDP calculator in C (server)
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	puts("!!!Hello World!!!"); /* prints !!!Hello World!!! */
+	return EXIT_SUCCESS;
+}
